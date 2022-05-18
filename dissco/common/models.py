f0ea3a4abf6929455007cc7b23c9246ec3025022
Josh Humphries <j.humphries@nhm.ac.uk>
2022-05-19 00:24:01 +0100

    Remove date fields from institution and remake migrations

--- dissco/common/models.py
+++ dissco/common/models.py
@@ -3,8 +3,6 @@ from django.db import models
 
 class Institution(models.Model):
     name = models.CharField(max_length=1000)
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
     code = models.TextField(max_length=255, blank=True)
     wikidata_id = models.CharField(max_length=255, blank=True)
 
