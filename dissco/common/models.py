ba6bef81c4131b0fc63f0b032f0f604090b40bec
Josh Humphries <j.humphries@nhm.ac.uk>
2022-05-18 23:54:34 +0100

    Switch to a static list of institutions, loaded from a fixture
    Also, remove ROR ID replacing it with wikidata. This is because we
    can get the ROR ID if we want it from wikidata easily and we can get
    more info there too.

--- dissco/common/models.py
+++ dissco/common/models.py
@@ -5,8 +5,8 @@ class Institution(models.Model):
     name = models.CharField(max_length=1000)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
-    code = models.CharField(max_length=10)
-    ror_id = models.CharField(max_length=9, blank=True)
+    code = models.TextField(max_length=255, blank=True)
+    wikidata_id = models.CharField(max_length=255, blank=True)
 
     def __str__(self) -> str:
         return f'{self.name} [{self.code}]'
