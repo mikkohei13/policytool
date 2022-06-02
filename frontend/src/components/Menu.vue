e34a7be9c4705b9c8717a13f78a0f587d78a8173
Josh Humphries <j.humphries@nhm.ac.uk>
2022-06-02 12:15:37 +0100

    Show the pointer cursor on the entire menu item, not just the link

--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -5,7 +5,7 @@
         <VueFeather type="menu" size="2rem"></VueFeather>
       </button>
     </div>
-    <ul v-if="visible" class="bg-white p-0 border-solid border-t-2 border-yellow">
+    <ul v-if="visible" class="bg-white p-0 border-solid border-t-2 border-yellow cursor-pointer">
       <li v-for="item in visibleItems" class="text-lg hover:bg-grey py-1 px-5">
         <a :href="item.link" @click="hide">
           {{ item.title }}
