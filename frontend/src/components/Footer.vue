f1489e87a149beb2798a43ea0cb055024a7ea0ed
Josh Humphries <j.humphries@nhm.ac.uk>
2022-06-02 18:21:37 +0100

    Remove text copied from dissco.eu and rearrange logos to the right

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,24 +1,21 @@
 <template>
-  <footer class="w-full p-6 flex-col bg-grey-light bottom-0 fixed text-blue-dark">
-    <div class="flex flex-col">
-      <a href="https://dissco.eu" target="_blank">DiSSCo
+  <footer class="w-full p-6 flex bg-grey-light bottom-0 fixed text-blue-dark">
+    <div class="flex flex-col justify-center">
+      <a href="https://dissco.eu" target="_blank" class="block py-1">DiSSCo
         <VueFeather type="external-link" size="1rem"/>
       </a>
-      <a href="https://github.com/DiSSCo/policytool" target="_blank">Source code
+      <a href="https://github.com/DiSSCo/policytool" target="_blank" class="block">Source code
         <VueFeather type="external-link" size="1rem"/>
       </a>
     </div>
-    <div class="grow text-center flex-col">
-      <div class="mx-auto flex justify-center">
-        <a href="https://joint-research-centre.ec.europa.eu" target="_blank">
-          <img src="/eu-logo.png" alt="european union" class="object-contain h-10">
-        </a>
-        <a href="https://www.eosc-portal.eu" target="_blank" title="EOSC ">
-          <img src="/eosc_logo.png" alt="eosc logo" class="object-contain h-10">
-        </a>
-      </div>
-      <p>H2020-INFRADEV-2019-2020 – Grant Agreement No. 871043.</p>
-      <p>Funded by the Horizon 2020 Framework Programme of the European Union</p>
+    <div class="grow"></div>
+    <div class="flex flex-row justify-center">
+      <a href="https://joint-research-centre.ec.europa.eu" target="_blank" class="place-self-center">
+        <img src="/eu-logo.png" alt="european union" class="object-contain h-16">
+      </a>
+      <a href="https://www.eosc-portal.eu" target="_blank" title="EOSC" class="place-self-center">
+        <img src="/eosc_logo.png" alt="eosc logo" class="object-contain h-20">
+      </a>
     </div>
   </footer>
 </template>
