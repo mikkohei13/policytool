49535c41245ad781a4f46db45faf1b89f6714815
Josh Humphries <j.humphries@nhm.ac.uk>
2022-06-02 12:28:31 +0100

    Route paths to non-existent components to NotFound for now

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,9 +4,9 @@ import NotFound from './views/NotFound.vue'
 /** @type {import('vue-router').RouterOptions['routes']} */
 export const routes = [
   { path: '/', component: Home, name: 'home', meta: { title: 'Home' } },
-  { path: '/dashboards', component: Home, name: 'dashboards', meta: { title: 'Dashboards' } },
-  { path: '/policies', component: Home, name: 'policies', meta: { title: 'Policies' } },
-  { path: '/institutions', component: Home, name: 'institutions', meta: { title: 'Institutions' } },
-  { path: '/login', component: Home, name: 'login', meta: { title: 'login' } },
+  { path: '/dashboards', component: NotFound, name: 'dashboards', meta: { title: 'Dashboards' } },
+  { path: '/policies', component: NotFound, name: 'policies', meta: { title: 'Policies' } },
+  { path: '/institutions', component: NotFound, name: 'institutions', meta: { title: 'Institutions' } },
+  { path: '/login', component: NotFound, name: 'login', meta: { title: 'login' } },
   { path: '/:path(.*)', component: NotFound },
 ]
