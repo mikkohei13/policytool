92a871db6edf640a5bb403d6a6bb1e39fc059ad9
Josh Humphries <j.humphries@nhm.ac.uk>
2022-06-03 13:19:37 +0100

    Add z index to menu

--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -5,7 +5,7 @@
         <VueFeather type="menu" size="2rem"></VueFeather>
       </button>
     </div>
-    <ul v-if="visible" class="bg-white p-0 border-solid border-t-2 border-yellow cursor-pointer">
+    <ul v-if="visible" class="bg-white p-0 border-solid border-t-2 border-yellow cursor-pointer z-10">
       <li v-for="item in visibleItems">
         <router-link :to="item.to" @click="hide" class="text-lg hover:bg-grey py-1 px-5 block w-full">
           {{ item.title }}
