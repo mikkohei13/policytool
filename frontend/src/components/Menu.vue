05b3c5e9113ed577f7cdd58385372ba2f082c05e
Josh Humphries <j.humphries@nhm.ac.uk>
2022-06-07 16:15:38 +0100

    Space the menu items better

--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -7,7 +7,7 @@
     </div>
     <ul v-if="visible" class="bg-white p-0 border-solid border-t-2 border-yellow cursor-pointer z-10">
       <li v-for="item in visibleItems">
-        <router-link :to="item.to" @click="hide" class="text-lg hover:bg-grey py-1 px-5 block w-full">
+        <router-link :to="item.to" @click="hide" class="text-lg hover:bg-grey first:pt-2 pb-2 px-8 block w-full">
           {{ item.title }}
         </router-link>
       </li>
