5fb075bbd5a6d8d1deec36e20d98e2863ddf4ef4
Josh Humphries <j.humphries@nhm.ac.uk>
2022-06-07 16:16:30 +0100

    Match the size of the home page

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto max-w-7xl p-6">
+  <div class="p-4 mx-auto max-w-screen-2xl">
     <h1 class="text-3xl font-bold">Page not found</h1>
   </div>
 </template>
