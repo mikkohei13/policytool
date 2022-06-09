6a9923570a883237ffda62eeece5c60f3a821aa7
Josh Humphries <j.humphries@nhm.ac.uk>
2022-06-09 15:59:51 +0100

    Fade the menu in

--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -3,23 +3,28 @@
     <button class="p-2 pb-1 rounded text-white hover:text-grey place-self-end">
       <VueFeather type="menu" size="2rem"></VueFeather>
     </button>
-    <ul v-if="visible" class="absolute right-0 top-full bg-white p-0 border-solid border-t-2 border-yellow cursor-pointer z-10">
-      <li v-for="item in visibleItems">
-        <router-link v-if="item.to" :to="item.to" @click="hide"
-                     class="text-lg hover:bg-grey first:pt-2 pb-2 px-8 block w-full">
-          {{ item.title }}
-        </router-link>
-        <span v-if="!item.to" @click="item.func" class="text-lg hover:bg-grey first:pt-2 pb-2 px-8 block w-full">
-          {{ item.title }}
-        </span>
-      </li>
-    </ul>
+    <Fade>
+      <ul v-if="visible"
+          class="absolute right-0 top-full bg-white p-0 border-solid border-t-2 border-yellow cursor-pointer z-10">
+        <li v-for="item in visibleItems">
+          <router-link v-if="item.to" :to="item.to" @click="hide"
+                       class="text-lg hover:bg-grey first:pt-2 pb-2 px-8 block w-full">
+            {{ item.title }}
+          </router-link>
+          <span v-if="!item.to" @click="item.func"
+                class="text-lg hover:bg-grey first:pt-2 pb-2 px-8 block w-full">
+            {{ item.title }}
+          </span>
+        </li>
+      </ul>
+    </Fade>
   </div>
 </template>
 
 <script setup>
 import {computed, ref} from 'vue'
 import VueFeather from 'vue-feather'
+import Fade from '@/components/transitions/Fade.vue'
 
 const {items} = defineProps(['items'])
 let visible = ref(false)
