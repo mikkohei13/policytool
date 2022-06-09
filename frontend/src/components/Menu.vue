8d33a3c89f73ee3bf94019a7c380aa0f33c5e330
Josh Humphries <j.humphries@nhm.ac.uk>
2022-06-09 14:40:32 +0100

    Allow menu items to be just text

--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -3,11 +3,16 @@
     <button class="p-2 pb-1 rounded text-white hover:text-grey place-self-end">
       <VueFeather type="menu" size="2rem"></VueFeather>
     </button>
-    <ul v-if="visible" class="bg-white p-0 border-solid border-t-2 border-yellow cursor-pointer z-10">
+    <ul v-if="visible"
+        class="bg-white p-0 border-solid border-t-2 border-yellow cursor-pointer z-10">
       <li v-for="item in visibleItems">
-        <router-link :to="item.to" @click="hide" class="text-lg hover:bg-grey first:pt-2 pb-2 px-8 block w-full">
+        <router-link v-if="item.to" :to="item.to" @click="hide"
+                     class="text-lg hover:bg-grey first:pt-2 pb-2 px-8 block w-full">
           {{ item.title }}
         </router-link>
+        <span v-if="!item.to" @click="item.func" class="text-lg hover:bg-grey first:pt-2 pb-2 px-8 block w-full">
+          {{ item.title }}
+        </span>
       </li>
     </ul>
   </div>
