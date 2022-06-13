1d2de9f4381a71c600263631620426cce69fcf8b
Josh Humphries <j.humphries@nhm.ac.uk>
2022-06-13 22:47:05 +0100

    Add start new button and do some button style adjustments

--- frontend/src/views/InstitutionHome.vue
+++ frontend/src/views/InstitutionHome.vue
@@ -6,14 +6,19 @@
   </div>
   <div class="flex flex-col md:flex-row gap-8">
     <div class="w-3/5 border-2 bg-white p-3">
-      <div class="flex flex-row pb-2 border-b-2">
+      <div class="flex flex-row gap-2 pb-2 border-b-2 border-grey-darker">
         <div class="text-black text-2xl">
           Review Policies
         </div>
         <div class="grow"></div>
-        <button class="bg-yellow border py-1 px-3">
+        <!-- TODO: create button component -->
+        <button class="bg-yellow border py-1 pr-3">
+          <VueFeather type="plus" size="1.4rem" class="align-middle mx-1 -mt-1"/>
+          Start new
+        </button>
+        <button class="bg-grey border py-1 pr-3">
+          <VueFeather type="chevron-down" size="1.4rem" class="align-middle mx-1 -mt-1"/>
           Filter
-          <VueFeather type="chevron-down" size="1.4rem" class="align-middle" />
         </button>
       </div>
       <div>
