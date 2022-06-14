27317fd5e2689ce2be459160a27aca946ef2430c
Josh Humphries <j.humphries@nhm.ac.uk>
2022-06-14 02:23:07 +0100

    Order options for consistency

--- dissco/policy/packs.py
+++ dissco/policy/packs.py
@@ -23,7 +23,7 @@ def to_pack(institution: Institution, policy_area: PolicyArea) -> Pack:
 
 def to_question(institution: Institution, policy_component: PolicyComponent) -> Question:
     # TODO: do we want to include the id as well as the value for each option?
-    options = [option.value for option in policy_component.options.all()]
+    options = [option.value for option in policy_component.options.all().order_by('id')]
     if not options:
         options = None
 
