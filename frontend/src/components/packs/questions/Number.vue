c7d86854f7ca1553082f9dc780b86a3ba38bbe75
Josh Humphries <j.humphries@nhm.ac.uk>
2022-06-15 23:22:19 +0100

    Add number question type

--- frontend/src/components/packs/questions/Number.vue
+++ frontend/src/components/packs/questions/Number.vue
@@ -1,6 +1,23 @@
 <template>
-
+  <div class="flex flex-col">
+    <input v-model="value" type="number" :id="inputId" class="w-full sm:w-40">
+    <textarea v-model="comment" :id="commentId" placeholder="Comments" class="mt-8"></textarea>
+  </div>
 </template>
 
 <script setup>
+import {ref, watch} from 'vue'
+
+const emit = defineEmits(['updateAnswer'])
+const {question} = defineProps(['question'])
+
+const value = ref(question.answer ? Number(question.answer.value) : null)
+const comment = ref(question.answer ? question.answer.comment : null)
+
+const inputId = `number-${question.id}`
+const commentId = `number-comment-${question.id}`
+
+watch([value, comment], ([newValue, newComment]) => {
+  emit('updateAnswer', question.id, newValue, newComment)
+})
 </script>
