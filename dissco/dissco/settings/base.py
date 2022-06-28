bd197df2c1890506ccc3d69075be67bdf4f74a06
Josh Humphries <j.humphries@nhm.ac.uk>
2022-06-28 01:55:18 +0100

    Add whitenoise config to base settings

--- dissco/dissco/settings/base.py
+++ dissco/dissco/settings/base.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,3 +131,5 @@ QA_PACK_PROVIDERS = {
     'policy': 'policy.packs.PolicyPackProvider',
     'maturity': 'maturity.packs.DigitalMaturityPackProvider',
 }
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
