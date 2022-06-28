ef5abbd2e190890b0e873e77af92f8f13cccbe06
Josh Humphries <j.humphries@nhm.ac.uk>
2022-06-28 03:42:35 +0100

    Group the questions by the order value
    This allows us to show the maturity questions where the text is the same but the period is different on the same page. I'd like to make this implementation better cause it's a bit of a hack.

--- frontend/src/views/Pack.vue
+++ frontend/src/views/Pack.vue
@@ -7,19 +7,19 @@
       <div class="grow"></div>
       <div class="flex flex-row gap-2 h-10">
         <div class="self-center pr-2 border-r-2">
-          {{ currentIndex + 1 }} / {{ pack.questions?.length }}
+          {{ groupIndex + 1 }} / {{ questionGroups.length }}
         </div>
         <div class="h-10 w-10">
           <button class="bg-grey p-1 w-full h-full rounded hover:bg-grey-dark" v-if="showPrevious">
             <VueFeather type="chevron-left" size="2rem"
-                        @click="currentIndex--" class="cursor-pointer"/>
+                        @click="groupIndex--" class="cursor-pointer"/>
           </button>
         </div>
         <div class="h-10 w-10">
           <button class="bg-grey p-1 w-full h-full rounded hover:bg-grey-dark p-1 h-10 w-10"
                   v-if="showNext">
             <VueFeather type="chevron-right" size="2rem" class="cursor-pointer"
-                        @click="currentIndex++"/>
+                        @click="groupIndex++"/>
           </button>
         </div>
         <button class="bg-yellow rounded hover:bg-yellow-dark p-1 h-10 w-10" @click="saveAnswers">
@@ -31,9 +31,9 @@
 
     <div class="px-12">
       <div class="grow justify-self-start">
-        <div v-for="(question, index) in pack.questions">
-          <div v-if="index === currentIndex" class="p-4">
-            <div class="text-xl pb-2 font-bold">
+        <div v-for="(question, index) in questionGroups[groupIndex]">
+          <div class="p-4">
+            <div class="text-xl pb-2 font-bold" v-if="index === 0">
               {{ question.text }}
             </div>
             <div class="pb-6">
@@ -73,21 +73,21 @@ const types = {
 const {id, type} = defineProps(['id', 'type'])
 const pack = ref({})
 const updatedQuestionIds = new Set()
-let currentIndex = ref(0)
+const questionGroups = ref([])
+const groupIndex = ref(0)
 
-const showPrevious = computed(() => currentIndex.value !== 0)
+const showPrevious = computed(() => groupIndex.value !== 0)
 const showNext = computed(() => {
-  if (!pack.value.questions) {
+  if (!questionGroups.value) {
     return false
   } else {
-    return currentIndex.value < pack.value.questions.length - 1
+    return groupIndex.value < questionGroups.value.length - 1
   }
 })
 
 const getQuestion = (questionId) => {
   return pack.value.questions.find((question) => question.id === questionId)
 }
-
 
 const saveAnswers = async () => {
   for (const questionId of updatedQuestionIds) {
@@ -99,12 +99,32 @@ const saveAnswers = async () => {
 
 const updatePack = async () => {
   pack.value = await api.get(`/api/${type}/pack/${id}`)
+
+  // group the questions by order value in an array
+  questionGroups.value = []
+  let order = 0
+  let group = []
+  for (const question of pack.value.questions) {
+    if (question.order === order) {
+      group.push(question)
+    } else {
+      order = question.order
+      if (group.length) {
+        questionGroups.value.push(group)
+      }
+      group = [question]
+    }
+  }
+  if (group.length) {
+    questionGroups.value.push(group)
+  }
 }
 
 onMounted(async () => {
   await updatePack()
+
   const index = pack.value.questions.findIndex((question) => !question.answer)
-  currentIndex.value = Math.max(index, 0)
+  groupIndex.value = Math.max(index, 0)
 })
 
 const updateAnswer = (questionId, value, comment) => {
