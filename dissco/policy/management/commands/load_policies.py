bf87a57469933ad3b04ba6dce07af5265e6f353d
Josh Humphries <j.humphries@nhm.ac.uk>
2022-07-14 14:47:37 +0100

    Change name from data to defs

--- dissco/policy/management/commands/load_policies.py
+++ dissco/policy/management/commands/load_policies.py
@@ -3,7 +3,7 @@ from django.core.management import BaseCommand
 from dissco.settings.base import BASE_DIR
 from policy.loading import load
 
-DEFAULT_DATA_DIR = BASE_DIR / 'policy' / 'data'
+DEFAULT_DATA_DIR = BASE_DIR / 'policy' / 'defs'
 
 
 class Command(BaseCommand):
