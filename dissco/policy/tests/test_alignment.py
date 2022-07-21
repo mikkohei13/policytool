84dec7ba7d1bc2c09640fd9858e6d646b9e6cf39
Josh Humphries <j.humphries@nhm.ac.uk>
2022-07-21 18:37:19 +0100

    Refactor alignment tests

--- dissco/policy/tests/test_alignment.py
+++ dissco/policy/tests/test_alignment.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+from functools import partial
 from uuid import uuid4
 
 import pytest
@@ -54,231 +56,116 @@ def test_number_equal(allowed_value: str, answer_value: str, ok: bool):
     assert result.passed == ok
 
 
-def create_mapping(options: list[str], allowed: list[str], component_type: PolicyComponentType,
-                   rule: Rule) -> ServicePolicyMapping:
-    def random_str() -> str:
-        return str(uuid4())
+@dataclass
+class Scenario:
+    type: PolicyComponentType
+    rule: Rule
+    allowed: list[str]
+    choices: list[str]
+    ok: bool
+    options: tuple[str] = ('a', 'b', 'c', 'd', 'e', 'f')
+    prefix: str = str(uuid4())
 
-    service = Service(name=random_str())
-    service.save()
+    def create_mapping(self) -> ServicePolicyMapping:
+        service = Service(name=f'{self.prefix}_service')
+        service.save()
 
-    service_component = ServiceComponent(name=random_str(), service=service)
-    service_component.save()
+        service_component = ServiceComponent(name=f'{self.prefix}_service_component',
+                                             service=service)
+        service_component.save()
 
-    policy_category = PolicyCategory(name=random_str())
-    policy_category.save()
+        policy_category = PolicyCategory(name=f'{self.prefix}_policy_category')
+        policy_category.save()
 
-    policy_area = PolicyArea(name=random_str(), number=1, category=policy_category)
-    policy_area.save()
+        policy_area = PolicyArea(name=f'{self.prefix}_policy_area', number=1,
+                                 category=policy_category)
+        policy_area.save()
 
-    policy_component = PolicyComponent(type=component_type, policy_area=policy_area)
-    policy_component.save()
+        policy_component = PolicyComponent(name=f'{self.prefix}_policy_component', type=self.type,
+                                           question=f'{self.prefix}_question',
+                                           policy_area=policy_area)
+        policy_component.save()
 
-    allowed_options = []
-    for value in options:
-        option = PolicyComponentOption(value=value, policy_component=policy_component)
-        if value in allowed:
-            allowed_options.append(option)
-        option.save()
-        policy_component.options.add(option)
-    policy_component.save()
+        allowed_options = []
+        for value in self.options:
+            option = PolicyComponentOption(value=value, policy_component=policy_component)
+            if value in self.allowed:
+                allowed_options.append(option)
+            option.save()
+            policy_component.options.add(option)
+        policy_component.save()
 
-    mapping = ServicePolicyMapping(rule=rule, service_component=service_component,
-                                   policy_component=policy_component)
-    mapping.save()
+        mapping = ServicePolicyMapping(rule=self.rule, service_component=service_component,
+                                       policy_component=policy_component)
+        mapping.save()
 
-    mapping.allowed_options.set(allowed_options)
-    mapping.save()
-    return mapping
+        mapping.allowed_options.set(allowed_options)
+        mapping.save()
+        return mapping
 
-
-@pytest.mark.django_db
-class TestOptionSingleEqual:
-
-    def test_no_choice(self):
-        mapping = create_mapping(['a', 'b', 'c', 'd'], ['b'],
-                                 PolicyComponentType.OPTION_SINGLE, Rule.EQUAL)
-
-        institution = Institution(name='test institution')
+    def create_institution(self) -> Institution:
+        institution = Institution(name=f'{self.prefix}_test_institution')
         institution.save()
-        answer = InstitutionPolicyComponent(institution=institution,
-                                            policy_component=mapping.policy_component)
-        answer.save()
-        result = validators.validate(mapping, answer)
-        assert not result.passed
-
-    def test_valid_choice(self):
-        mapping = create_mapping(['a', 'b', 'c', 'd'], ['b'],
-                                 PolicyComponentType.OPTION_SINGLE, Rule.EQUAL)
-
-        institution = Institution(name='test institution')
-        institution.save()
-        answer = InstitutionPolicyComponent(institution=institution,
-                                            policy_component=mapping.policy_component)
-        answer.save()
-        answer.chosen_options.add(mapping.policy_component.options.all()[1])
-        answer.save()
-        result = validators.validate(mapping, answer)
-        assert result.passed
-
-    def test_invalid_choice(self):
-        mapping = create_mapping(['a', 'b', 'c', 'd'], ['b'],
-                                 PolicyComponentType.OPTION_SINGLE, Rule.EQUAL)
-
-        institution = Institution(name='test institution')
-        institution.save()
-        answer = InstitutionPolicyComponent(institution=institution,
-                                            policy_component=mapping.policy_component)
-        answer.save()
-        answer.chosen_options.add(mapping.policy_component.options.all()[0])
-        answer.save()
-        result = validators.validate(mapping, answer)
-        assert not result.passed
+        return institution
 
 
-option_single_equal_scenarios = [
+OptionSingleEqual = partial(Scenario, type=PolicyComponentType.OPTION_SINGLE, rule=Rule.EQUAL)
+OptionSingleOr = partial(Scenario, type=PolicyComponentType.OPTION_SINGLE, rule=Rule.OR)
+OptionMultipleEqual = partial(Scenario, type=PolicyComponentType.OPTION_MULTIPLE, rule=Rule.EQUAL)
+OptionMultipleOr = partial(Scenario, type=PolicyComponentType.OPTION_MULTIPLE, rule=Rule.OR)
+
+scenarios = [
     # no answer
-    (['a', 'b'], ['a'], '', False),
-    # a valid answer
-    (['a', 'b'], ['a'], 'a', True),
-    # an invalid answer
-    (['a', 'b'], ['a'], 'b', False),
+    OptionSingleEqual(allowed=['a'], choices=[], ok=False),
+    # valid answer
+    OptionSingleEqual(allowed=['a'], choices=['a'], ok=True),
+    # invalid answer
+    OptionSingleEqual(allowed=['a'], choices=['b'], ok=False),
+
+    # no answer
+    OptionSingleOr(allowed=['a', 'c'], choices=[], ok=False),
+    # valid answer
+    OptionSingleOr(allowed=['a', 'c'], choices=['a'], ok=True),
+    # another valid answer
+    OptionSingleOr(allowed=['a', 'c'], choices=['c'], ok=True),
+    # invalid answer
+    OptionSingleOr(allowed=['a', 'c'], choices=['b'], ok=False),
+
+    # no answer
+    OptionMultipleEqual(allowed=['a', 'c'], choices=[], ok=False),
+    # valid answer
+    OptionMultipleEqual(allowed=['a', 'c'], choices=['a', 'c'], ok=True),
+    # invalid answer (just wrong)
+    OptionMultipleEqual(allowed=['a', 'c'], choices=['d'], ok=False),
+    # invalid answer (partially just wrong)
+    OptionMultipleEqual(allowed=['a', 'c'], choices=['a', 'd'], ok=False),
+    # invalid answer (subset is right)
+    OptionMultipleEqual(allowed=['a', 'c'], choices=['a', 'b', 'c'], ok=False),
+
+    # no answer
+    OptionMultipleOr(allowed=['a', 'c'], choices=[], ok=False),
+    # valid answer (full match)
+    OptionMultipleOr(allowed=['a', 'c'], choices=['a', 'c'], ok=True),
+    # valid answer (partial match)
+    OptionMultipleOr(allowed=['a', 'c'], choices=['c'], ok=True),
+    # valid answer (partial match + mismatch)
+    OptionMultipleOr(allowed=['a', 'c'], choices=['a', 'b', 'c'], ok=True),
+    # invalid answer
+    OptionMultipleOr(allowed=['a', 'c'], choices=['d'], ok=False),
 ]
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrize('options, allowed_options, choice, ok', option_single_equal_scenarios)
-def test_option_single_equal(options: list[str], allowed_options: list[str], choice: str, ok: bool):
-    mapping = create_mapping(options, allowed_options, PolicyComponentType.OPTION_SINGLE,
-                             Rule.EQUAL)
-    institution = Institution(name='test institution')
-    institution.save()
-    answer = InstitutionPolicyComponent(institution=institution,
+@pytest.mark.parametrize('scenario', scenarios)
+def test_option_scenarios(scenario: Scenario):
+    mapping = scenario.create_mapping()
+
+    answer = InstitutionPolicyComponent(institution=scenario.create_institution(),
                                         policy_component=mapping.policy_component)
     answer.save()
     answer.chosen_options.set([option for option in mapping.policy_component.options.all()
-                               if option.value == choice])
+                               if option.value in scenario.choices])
     answer.save()
 
     result = validators.validate(mapping, answer)
-    assert result.passed == ok
-
-
-option_single_equal_scenarios = [
-    # no answer
-    (['a', 'b'], ['a'], '', False),
-    # a valid answer
-    (['a', 'b'], ['a'], 'a', True),
-    # an invalid answer
-    (['a', 'b'], ['a'], 'b', False),
-]
-
-
-@pytest.mark.django_db
-@pytest.mark.parametrize('options, allowed_options, choice, ok', option_single_equal_scenarios)
-def test_option_single_equal(options: list[str], allowed_options: list[str], choice: str, ok: bool):
-    mapping = create_mapping(options, allowed_options, PolicyComponentType.OPTION_SINGLE,
-                             Rule.EQUAL)
-    institution = Institution(name='test institution')
-    institution.save()
-    answer = InstitutionPolicyComponent(institution=institution,
-                                        policy_component=mapping.policy_component)
-    answer.save()
-    answer.chosen_options.set([option for option in mapping.policy_component.options.all()
-                               if option.value == choice])
-    answer.save()
-
-    result = validators.validate(mapping, answer)
-    assert result.passed == ok
-
-
-option_single_or_scenarios = [
-    # no answer
-    (['a', 'b'], ['a'], '', False),
-    # a valid answer
-    (['a', 'b', 'c', 'd'], ['a', 'c'], 'c', True),
-    # an invalid answer
-    (['a', 'b'], ['a', 'd'], 'b', False),
-]
-
-
-@pytest.mark.django_db
-@pytest.mark.parametrize('options, allowed_options, choice, ok', option_single_or_scenarios)
-def test_option_single_or(options: list[str], allowed_options: list[str], choice: str, ok: bool):
-    mapping = create_mapping(options, allowed_options, PolicyComponentType.OPTION_SINGLE,
-                             Rule.OR)
-    institution = Institution(name='test institution')
-    institution.save()
-    answer = InstitutionPolicyComponent(institution=institution,
-                                        policy_component=mapping.policy_component)
-    answer.save()
-    answer.chosen_options.set([option for option in mapping.policy_component.options.all()
-                               if option.value == choice])
-    answer.save()
-
-    result = validators.validate(mapping, answer)
-    assert result.passed == ok
-
-
-option_multiple_equal_scenarios = [
-    # no answer
-    (['a', 'b'], ['a'], [], False),
-    # a valid answer
-    (['a', 'b', 'c', 'd'], ['a', 'c'], ['a', 'c'], True),
-    # an invalid answer (mismatch)
-    (['a', 'b', 'c', 'd'], ['a', 'c'], ['a', 'b'], False),
-    # an invalid answer (subset + mismatch)
-    (['a', 'b', 'c', 'd'], ['a', 'c'], ['a', 'b', 'c'], False),
-]
-
-
-@pytest.mark.django_db
-@pytest.mark.parametrize('options, allowed_options, choices, ok', option_multiple_equal_scenarios)
-def test_option_multiple_equal(options: list[str], allowed_options: list[str], choices: list[str],
-                               ok: bool):
-    mapping = create_mapping(options, allowed_options, PolicyComponentType.OPTION_MULTIPLE,
-                             Rule.EQUAL)
-    institution = Institution(name='test institution')
-    institution.save()
-    answer = InstitutionPolicyComponent(institution=institution,
-                                        policy_component=mapping.policy_component)
-    answer.save()
-    answer.chosen_options.set([option for option in mapping.policy_component.options.all()
-                               if option.value in choices])
-    answer.save()
-
-    result = validators.validate(mapping, answer)
-    assert result.passed == ok
-
-
-option_multiple_or_scenarios = [
-    # no answer
-    (['a', 'b'], ['a'], [], False),
-    # a valid answer (full match)
-    (['a', 'b', 'c', 'd'], ['a', 'c'], ['a', 'c'], True),
-    # a valid answer (partial match)
-    (['a', 'b', 'c', 'd'], ['a', 'c'], ['c'], True),
-    # a valid answer (partial + mismatch)
-    (['a', 'b', 'c', 'd'], ['a', 'c'], ['a', 'b', 'c'], True),
-    # an invalid answer (mismatch)
-    (['a', 'b', 'c', 'd'], ['a', 'c'], ['b'], False),
-]
-
-
-@pytest.mark.django_db
-@pytest.mark.parametrize('options, allowed_options, choices, ok', option_multiple_or_scenarios)
-def test_option_multiple_or(options: list[str], allowed_options: list[str], choices: list[str],
-                            ok: bool):
-    mapping = create_mapping(options, allowed_options, PolicyComponentType.OPTION_MULTIPLE,
-                             Rule.OR)
-    institution = Institution(name='test institution')
-    institution.save()
-    answer = InstitutionPolicyComponent(institution=institution,
-                                        policy_component=mapping.policy_component)
-    answer.save()
-    answer.chosen_options.set([option for option in mapping.policy_component.options.all()
-                               if option.value in choices])
-    answer.save()
-
-    result = validators.validate(mapping, answer)
-    assert result.passed == ok
+    assert result.passed == scenario.ok
