72b5df6d90cf6186bf2446d9b7acb7f04fa62bd6
Josh Humphries <j.humphries@nhm.ac.uk>
2022-07-27 17:35:11 +0100

    Add team name validation

--- dissco/maturity/serializers.py
+++ dissco/maturity/serializers.py
@@ -1,9 +1,23 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from maturity import models
+
+
+TEAM_NAME_UNIQUENESS_ERROR = '''The name must be unique among your existing teams (this applies 
+regardless of the type of team)'''
 
 
 class ResponderSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Responder
         fields = ['id', 'type', 'name', 'comment', 'owner']
+
+    def validate(self, data):
+        name: str = data.get('name')
+        owner: User = data.get('owner')
+
+        if models.Responder.objects.filter(owner=owner, name=name).exists():
+            raise serializers.ValidationError({'name': TEAM_NAME_UNIQUENESS_ERROR})
+
+        return super().validate(data)
